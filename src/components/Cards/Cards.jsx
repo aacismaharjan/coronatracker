b34f34276ca71d3d45c47805c1f176291b9c6836
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-10-08 21:36:25 +0545

    Uninstall necessary packages & optimized code

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,39 +1,36 @@
 import React from 'react'
 import Card from './Card'
 import Loading from '../Loading'
-import { findByLabelText } from '@testing-library/react'
 
-const Cards = (props) => {
-  const { lastUpdate, recovered, confirmed, deaths } = props.data
+const Cards = ({ data: { lastUpdate, recovered, confirmed, deaths } }) => {
+  const cardItems = confirmed
+    ? [
+        {
+          name: 'Infected',
+          text: 'Number of active cases of COVID-19',
+          cases: recovered.value,
+        },
+        {
+          name: 'Recovered',
+          text: 'Number of active cases of COVID-19',
+          cases: confirmed.value,
+        },
+        {
+          name: 'Deaths',
+          text: 'Number of active cases of COVID-19',
+          cases: deaths.value,
+        },
+      ]
+    : []
 
-  const cardItems = confirmed && [
-    {
-      name: 'Infected',
-      text: 'Number of active cases of COVID-19',
-      cases: recovered.value,
-    },
-    {
-      name: 'Recovered',
-      text: 'Number of active cases of COVID-19',
-      cases: confirmed.value,
-    },
-    {
-      name: 'Deaths',
-      text: 'Number of active cases of COVID-19',
-      cases: deaths.value,
-    },
-  ]
+  const cardsContainer = cardItems.map((card, index) => (
+    <Card cards={{ ...card, lastUpdate }} key={index} />
+  ))
 
   return (
     <div className='container my-5'>
       <div className='row'>
-        {cardItems ? (
-          cardItems.map((card, index) => (
-            <Card cards={{ ...card, lastUpdate }} key={index} />
-          ))
-        ) : (
-          <Loading />
-        )}
+        {cardItems.length ? cardsContainer : <Loading />}
       </div>
     </div>
   )

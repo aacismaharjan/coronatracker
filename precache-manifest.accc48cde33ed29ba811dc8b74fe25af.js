950ddd3b1ae849c621ee553bb44485da00a28b74
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-10-08 21:15:43 +0545

    Updates

--- precache-manifest.accc48cde33ed29ba811dc8b74fe25af.js
+++ precache-manifest.accc48cde33ed29ba811dc8b74fe25af.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0611e0569483020de04cbcc5e4f88aae",
+    "revision": "d8e213578ef7695f4d0d40acf793be38",
     "url": "/coronatracker/index.html"
   },
   {
-    "revision": "2c0af981689c583c8221",
+    "revision": "4d488fa89101049b08a8",
     "url": "/coronatracker/static/css/main.14ea13af.chunk.css"
   },
   {
-    "revision": "43cfb5c4e9a4838de2bc",
-    "url": "/coronatracker/static/js/2.14815b40.chunk.js"
+    "revision": "7789e9b1c2d87b386901",
+    "url": "/coronatracker/static/js/2.abdcd55a.chunk.js"
   },
   {
     "revision": "b89c6e028c7ae0de5475b0003782bec7",
-    "url": "/coronatracker/static/js/2.14815b40.chunk.js.LICENSE.txt"
+    "url": "/coronatracker/static/js/2.abdcd55a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2c0af981689c583c8221",
-    "url": "/coronatracker/static/js/main.cbc2d0a7.chunk.js"
+    "revision": "4d488fa89101049b08a8",
+    "url": "/coronatracker/static/js/main.9dda5b19.chunk.js"
   },
   {
     "revision": "55eb73d4fba70127f3c0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coronatracker/precache-manifest.06f6327cb60799dcec00126724046c37.js"
+  "/coronatracker/precache-manifest.accc48cde33ed29ba811dc8b74fe25af.js"
 );
 
 self.addEventListener('message', (event) => {
